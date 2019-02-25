0131048222c4e13b5717ae67ef8dee5b6a7fe7a2
celien.donze <celien.donze@gmail.com>
2019-02-25 21:39:32 +0100

    add group and user, sort directory content by name

---
+++ resources/scripts/classes/Logic/Group.js
@@ -0,0 +1,28 @@
+/**
+ * class Group
+ * represents a group of users
+ */
+class Group {
+    constructor(name) {
+        this.name = name;
+
+        this.users = [];
+    }
+
+    /**
+     * addUser
+     * adds a user to the group
+     * @param {User} user 
+     */
+    addUser(user) {
+        this.users.push(user);
+    }
+
+    /**
+     * getName
+     * returns the name of the group
+     */
+    getName() {
+        return this.name;
+    }
+}
